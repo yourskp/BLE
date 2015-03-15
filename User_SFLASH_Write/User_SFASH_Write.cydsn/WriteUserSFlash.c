8c99e20ba2da6e1a305dd2d779a84d43d9da4d38
Krishna <yourskp@gmail.com>
2015-03-16 00:41:56 +0530

    User SFLASH read and write example project
    - Configure UART console with 115200 baud for program status and user
    messages
    - Hold down SW2 on 042-BLE Baseboard during reset to write user SFLASH
    with some preconfigured data
    - After device reset, displays the content of the user SFLASH locations
    (512 bytes) on the UART console.
    
    Look at the project schematic page for organization of the SFLASH in
    PSoC 4 BLE device.

---
+++ User_SFLASH_Write/User_SFASH_Write.cydsn/WriteUserSFlash.c
@@ -0,0 +1,101 @@
+/*******************************************************************************
+* File Name: WriteUserSFlash.c
+*
+* Version: 1.0
+*
+* Description:
+*  This file contains the source code for User SFLASH Write API.
+*
+* Owner:
+*  sey@cypress.com, kris@cypress.com
+*
+* Hardware Dependency:
+*  1. PSoC 4 BLE device
+*
+* Code Tested With:
+*  1. PSoC Creator 3.1 SP1
+*  2. ARM GCC 4.8.4
+*
+********************************************************************************
+* Copyright 2015, Cypress Semiconductor Corporation.  All rights reserved.
+* You may use this file only in accordance with the license, terms, conditions,
+* disclaimers, and limitations in the end user license agreement accompanying
+* the software package with which this file was provided.
+*******************************************************************************/
+
+#include "WriteUserSFlash.h"
+
+uint32 WriteUserSFlashRow(uint8 userrow_num, uint32 *write_value)
+{
+    uint8 local_count;
+	uint32 retvalue=0;
+	uint32 sram_cmd_data_buffer[(CY_FLASH_SIZEOF_ROW/4) + 2];
+	uint32 reg1,reg2,reg3,reg4,reg5,reg6;
+	
+	/* Store the clock settings temporarily */
+    reg1 =	CY_GET_XTND_REG32((void CYFAR *)(CYREG_CLK_SELECT));
+    reg2 =  CY_GET_XTND_REG32((void CYFAR *)(CYREG_CLK_IMO_CONFIG));
+    reg3 =  CY_GET_XTND_REG32((void CYFAR *)(CYREG_PWR_BG_TRIM4));
+    reg4 =  CY_GET_XTND_REG32((void CYFAR *)(CYREG_PWR_BG_TRIM5));
+    reg5 =  CY_GET_XTND_REG32((void CYFAR *)(CYREG_CLK_IMO_TRIM1));
+    reg6 =  CY_GET_XTND_REG32((void CYFAR *)(CYREG_CLK_IMO_TRIM2));
+	
+	/* Initialize the clock necessary for flash programming */
+	CY_SET_REG32(CYREG_CPUSS_SYSARG, 0x0000e8b6);
+	CY_SET_REG32(CYREG_CPUSS_SYSREQ, 0x80000015);
+	
+	/******* Initialize SRAM parameters for the LOAD FLASH command ******/
+	/* byte 3 (i.e. 00) is the Macro_select */
+	/* byte 2 (i.e. 00) is the Start addr of page latch */
+	/* byte 1 (i.e. d7) is the key 2  */
+	/* byte 0 (i.e. b6) is the key 1  */
+  	sram_cmd_data_buffer[0]=0x0000d7b6;
+	
+	/****** Initialize SRAM parameters for the LOAD FLASH command ******/
+	/* byte 3,2 and 1 are null */
+	/* byte 0 (i.e. 7F) is the number of bytes to be written */
+	sram_cmd_data_buffer[1]=0x0000007F;	 
+    
+	/* Initialize the SRAM buffer with data bytes */
+    for(local_count = 0; local_count < (CY_FLASH_SIZEOF_ROW/4); local_count++)    
+	{
+		sram_cmd_data_buffer[local_count + 2] = write_value[local_count]; 
+	}
+	
+	/* Write the following to registers to execute a LOAD FLASH bytes */
+	CY_SET_REG32(CYREG_CPUSS_SYSARG, sram_cmd_data_buffer);
+	CY_SET_REG32(CYREG_CPUSS_SYSREQ, LOAD_FLASH);
+	
+    /****** Initialize SRAM parameters for the WRITE ROW command ******/
+	/* byte 3 & 2 are null */
+	/* byte 1 (i.e. 0xeb) is the key 2  */
+	/* byte 0 (i.e. 0xb6) is the key 1  */
+	sram_cmd_data_buffer[0] = 0x0000ebb6;
+    
+	/* byte 7,6 and 5 are null */
+	/* byte 4 is desired SFLASH user row 
+	 * Allowed values 0 - row 4
+	                  1 - row 5 
+					  2 - row 6
+					  3 - row 7 */
+	sram_cmd_data_buffer[1] = (uint32) userrow_num;
+	
+	/* Write the following to registers to execute a WRITE USER SFLASH ROW command */
+	CY_SET_REG32(CYREG_CPUSS_SYSARG, sram_cmd_data_buffer);
+	CY_SET_REG32(CYREG_CPUSS_SYSREQ, WRITE_USER_SFLASH_ROW);
+	
+	/* Read back SYSARG for the result. 0xA0000000 = SUCCESS; */
+	retvalue = CY_GET_REG32(CYREG_CPUSS_SYSARG);
+	
+	/* Restore the clock settings after the flash programming is done */
+    reg1 =	CY_SET_XTND_REG32((void CYFAR *)(CYREG_CLK_SELECT),reg1);
+    reg2 =  CY_SET_XTND_REG32((void CYFAR *)(CYREG_CLK_IMO_CONFIG),reg2);
+    reg3 =  CY_SET_XTND_REG32((void CYFAR *)(CYREG_PWR_BG_TRIM4),reg3);
+    reg4 =  CY_SET_XTND_REG32((void CYFAR *)(CYREG_PWR_BG_TRIM5),reg4);
+    reg5 =  CY_SET_XTND_REG32((void CYFAR *)(CYREG_CLK_IMO_TRIM1),reg5);
+    reg6 =  CY_SET_XTND_REG32((void CYFAR *)(CYREG_CLK_IMO_TRIM2),reg6);  
+	
+	return retvalue;
+}
+
+/* [] END OF FILE */
---
+++ User_SFLASH_Write/User_SFASH_Write.cydsn/main.c
@@ -0,0 +1,128 @@
+/*******************************************************************************
+* File Name: main.c
+*
+* Version: 1.0
+*
+* Description:
+*  This project demonstrates how to read and write user SFLASH locations from
+*  firmware.
+*
+* Owner:
+*  kris@cypress.com
+*
+* Hardware Dependency:
+*  1. PSoC 4 BLE device
+*
+* Code Tested With:
+*  1. PSoC Creator 3.1 SP1
+*  2. ARM GCC 4.8.4
+*
+********************************************************************************
+* Copyright 2015, Cypress Semiconductor Corporation.  All rights reserved.
+* You may use this file only in accordance with the license, terms, conditions,
+* disclaimers, and limitations in the end user license agreement accompanying
+* the software package with which this file was provided.
+*******************************************************************************/
+
+#include <project.h>
+#include <WriteUserSFlash.h>
+
+char HexToAscii(uint8 value, uint8 nibble);
+
+#define SWITCH_PRESSED                  (0u)
+#define USER_SFLASH_ROW_SIZE            (128u)
+#define SFLASH_STARTING_VALUE           (0x00)
+#define USER_SFLASH_ROWS                (4u)
+#define USER_SFLASH_BASE_ADDRESS        (0x0ffff200u)
+
+int main()
+{
+    uint32 data[USER_SFLASH_ROW_SIZE/4];
+    uint8 *sflashPtr;
+    uint8 rowIndex;
+    uint32 dataIndex;
+            
+    CyGlobalIntEnable;
+    
+    UART_Console_Start();
+    
+    if(Write_Switch_Read() == SWITCH_PRESSED)
+    {
+        for(dataIndex = 0; dataIndex < (USER_SFLASH_ROW_SIZE/4); dataIndex++)
+        {
+            data[dataIndex] = SFLASH_STARTING_VALUE + dataIndex;  
+        }
+        
+        for(rowIndex = 0; rowIndex < USER_SFLASH_ROWS; rowIndex++)
+        {
+            UART_Console_UartPutString("\r\nWriting user SFLASH row");
+            UART_Console_UartPutChar('0' + rowIndex);
+            WriteUserSFlashRow(rowIndex, &data[0]);
+        }
+        UART_Console_UartPutString("\r\nUser SFLASH write complete\r\n");
+    }
+    
+    sflashPtr = (uint8 *)USER_SFLASH_BASE_ADDRESS;
+
+    /* Read all the 512 bytes of user configurable SFLASH content and display on UART console */
+    for(rowIndex = 0; rowIndex < USER_SFLASH_ROWS; rowIndex++)
+    {
+        UART_Console_UartPutString("\r\n\nUser SFLASH row ");
+        UART_Console_UartPutChar('0' + rowIndex);
+        UART_Console_UartPutString(" data:\r\n");
+        
+        for(dataIndex = 0; dataIndex < USER_SFLASH_ROW_SIZE; dataIndex++)
+        {
+            uint8 readData;
+            
+            readData = *sflashPtr++;
+            
+            UART_Console_UartPutChar(HexToAscii(readData,1));
+            UART_Console_UartPutChar(HexToAscii(readData,0));
+            UART_Console_UartPutChar(' ');
+        }
+    }
+
+    while(1);
+}
+
+char HexToAscii(uint8 value, uint8 nibble)
+{
+    if(nibble == 1)
+    {
+        value = value & 0xf0;
+        value = value >> 4;
+        
+        /* bit-shift the result to the right by four bits (i.e. quickly divides by 16)*/
+        if (value >9)
+        {
+            value = value - 10 + 'A';
+        }
+        else
+        {
+            value = value + '0';
+        }
+    }
+    else if (nibble == 0)
+    {
+        /* means use a bitwise AND to take the bottom four bits from the byte,
+        0x0F is 00001111 in binary*/
+        value = value & 0x0F;
+        if (value >9)
+        {
+            value = value - 10 + 'A';
+        }
+        else
+        {
+            value = value + '0';
+        }
+    }
+    else
+    {
+        value = ' ';    
+    }
+    
+    return value;
+}
+
+/* [] END OF FILE */
