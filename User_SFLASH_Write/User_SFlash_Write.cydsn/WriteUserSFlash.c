3bf26bad0f0581252ce9c3839cf54ae89fe8e810
Krishna <yourskp@gmail.com>
2015-03-29 00:50:24 +0530

    User SFlash row write example project submitted to 100 projects list.
    Code updates and cleanup for 100 projects submission

--- User_SFLASH_Write/User_SFlash_Write.cydsn/WriteUserSFlash.c
+++ User_SFLASH_Write/User_SFlash_Write.cydsn/WriteUserSFlash.c
@@ -4,7 +4,7 @@
 * Version: 1.0
 *
 * Description:
-*  This file contains the source code for User SFLASH Write API.
+*  This file contains the source code for User SFlash Write API.
 *
 * Owner:
 *  sey@cypress.com, kris@cypress.com
@@ -26,14 +26,31 @@
 #include <Project.h>
 #include <WriteUserSFlash.h>
 
+
+/*******************************************************************************
+* Function Name: WriteUserSFlashRow
+********************************************************************************
+* Summary:
+*        This routine calls the PSoC 4 BLE device supervisory ROM APIs to update
+* the user configuration area of Supervisory Flash (SFlash).  
+*
+* Parameters:
+*  userRowNUmber - User config SFlash row number to which data is to be written
+*  dataPointer - Pointer to the data to be written. This API writes one row of
+*                user config SFlash row at a time.
+*
+* Return:
+*  uint32 - state of the user config SFlash write operation.
+*
+*******************************************************************************/
 #if defined (__GNUC__)
 #pragma GCC optimize ("O0")
-#endif    
-uint32 WriteUserSFlashRow(uint8 userrow_num, uint32 *write_value)
+#endif /* End of #if defined (__GNUC__) */
+uint32 WriteUserSFlashRow(uint8 userRowNUmber, uint32 *dataPointer)
 {
-    uint8 local_count;
-	volatile uint32 retvalue=0;
-	volatile uint32 sram_cmd_data_buffer[(CY_FLASH_SIZEOF_ROW/4) + 2];
+    uint8 localCount;
+	volatile uint32 retValue=0;
+	volatile uint32 cmdDataBuffer[(CY_FLASH_SIZEOF_ROW/4) + 2];
 	volatile uint32 reg1,reg2,reg3,reg4,reg5,reg6;
 	
 	/* Store the clock settings temporarily */
@@ -53,43 +70,43 @@ uint32 WriteUserSFlashRow(uint8 userrow_num, uint32 *write_value)
 	/* byte 2 (i.e. 00) is the Start addr of page latch */
 	/* byte 1 (i.e. d7) is the key 2  */
 	/* byte 0 (i.e. b6) is the key 1  */
-  	sram_cmd_data_buffer[0]=0x0000d7b6;
+  	cmdDataBuffer[0]=0x0000d7b6;
 	
 	/****** Initialize SRAM parameters for the LOAD FLASH command ******/
 	/* byte 3,2 and 1 are null */
 	/* byte 0 (i.e. 7F) is the number of bytes to be written */
-	sram_cmd_data_buffer[1]=0x0000007F;	 
+	cmdDataBuffer[1]=0x0000007F;	 
     
 	/* Initialize the SRAM buffer with data bytes */
-    for(local_count = 0; local_count < (CY_FLASH_SIZEOF_ROW/4); local_count++)    
+    for(localCount = 0; localCount < (CY_FLASH_SIZEOF_ROW/4); localCount++)    
 	{
-		sram_cmd_data_buffer[local_count + 2] = write_value[local_count]; 
+		cmdDataBuffer[localCount + 2] = dataPointer[localCount]; 
 	}
 	
 	/* Write the following to registers to execute a LOAD FLASH bytes */
-	CY_SET_REG32(CYREG_CPUSS_SYSARG, &sram_cmd_data_buffer[0]);
+	CY_SET_REG32(CYREG_CPUSS_SYSARG, &cmdDataBuffer[0]);
 	CY_SET_REG32(CYREG_CPUSS_SYSREQ, LOAD_FLASH);
 	
     /****** Initialize SRAM parameters for the WRITE ROW command ******/
 	/* byte 3 & 2 are null */
 	/* byte 1 (i.e. 0xeb) is the key 2  */
 	/* byte 0 (i.e. 0xb6) is the key 1  */
-	sram_cmd_data_buffer[0] = 0x0000ebb6;
+	cmdDataBuffer[0] = 0x0000ebb6;
     
 	/* byte 7,6 and 5 are null */
-	/* byte 4 is desired SFLASH user row 
+	/* byte 4 is desired SFlash user row 
 	 * Allowed values 0 - row 4
 	                  1 - row 5 
 					  2 - row 6
 					  3 - row 7 */
-	sram_cmd_data_buffer[1] = (uint32) userrow_num;
+	cmdDataBuffer[1] = (uint32) userRowNUmber;
 	
-	/* Write the following to registers to execute a WRITE USER SFLASH ROW command */
-	CY_SET_REG32(CYREG_CPUSS_SYSARG, &sram_cmd_data_buffer[0]);
+	/* Write the following to registers to execute a WRITE USER SFlash ROW command */
+	CY_SET_REG32(CYREG_CPUSS_SYSARG, &cmdDataBuffer[0]);
 	CY_SET_REG32(CYREG_CPUSS_SYSREQ, WRITE_USER_SFLASH_ROW);
     
 	/* Read back SYSARG for the result. 0xA0000000 = SUCCESS; */
-	retvalue = CY_GET_REG32(CYREG_CPUSS_SYSARG);
+	retValue = CY_GET_REG32(CYREG_CPUSS_SYSARG);
 	
 	/* Restore the clock settings after the flash programming is done */
     CY_SET_XTND_REG32((void CYFAR *)(CYREG_CLK_SELECT),reg1);
@@ -99,10 +116,10 @@ uint32 WriteUserSFlashRow(uint8 userrow_num, uint32 *write_value)
     CY_SET_XTND_REG32((void CYFAR *)(CYREG_CLK_IMO_TRIM1),reg5);
     CY_SET_XTND_REG32((void CYFAR *)(CYREG_CLK_IMO_TRIM2),reg6);  
 	
-	return retvalue;
+	return retValue;
 }
 #if defined (__GNUC__)
 #pragma GCC reset_options
-#endif    
+#endif /* End of #if defined (__GNUC__) */
 
 /* [] END OF FILE */
